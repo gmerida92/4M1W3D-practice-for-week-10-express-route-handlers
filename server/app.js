8737785742d6d48942435cffaa9a6d05efa749f5
Brian Moore <runedelamont@gmail.com>
2022-06-01 19:36:43 -0700

    First commit

--- server/app.js
+++ server/app.js
@@ -23,8 +23,44 @@ const {
 
 const express = require('express');
 const app = express();
+app.use(express.json())
 
-// Your code here
+app.get('/artists/:artistId', (req, res) => {
+  const { artistId } = req.params;
+  // const artistId = req.params.artistId;
+
+  const artist = getArtistByArtistId(artistId);
+
+  res.status(200);
+  return res.json(artist);
+});
+
+app.put('/artists/:artistId', (req, res) => {
+
+  const { artistId } = req.params;
+  const { name } = req.body;
+
+  editArtistByArtistId(artistId, name);
+
+  res.status(200);
+  // plain text
+  res.send();
+
+  // parse json object
+  res.json({
+    name
+  })
+})
+
+app.get('/artists', (req, res) => {
+
+  const artists = getAllArtists();
+  res.status(200);
+  res.json(artists);
+
+});
+
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
